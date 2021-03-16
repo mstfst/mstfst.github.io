b025e0f9cb653d766858c92240d1aa6231613fbe
DESKTOP-U235624\fractal <gamesthatcan@gmail.com>
2021-03-15 22:18:55 -0400

    new build with proper homepage setting

--- react-tinder-card-example/service-worker.js
+++ react-tinder-card-example/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-tinder-card-demo/precache-manifest.e41daf638297cc1b7cd8dc05f56be79b.js"
+  "./precache-manifest.b501da8fbd3c2ab8f58ac4a9dbb288fb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-tinder-card-demo/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
