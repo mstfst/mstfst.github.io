a45ba87599fde56c9e5083d2fbc8a002dc52f0fe
DESKTOP-U235624\fractal <gamesthatcan@gmail.com>
2021-03-21 11:08:19 -0400

    updated build

---
+++ react-tinder-card-example/precache-manifest.36bbe885e50d4eb16a5c93978711ad8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c170608c9263017ae21fd743c9751ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "29b7625fe7fa5ca90218",
+    "url": "./static/css/main.85d0f105.chunk.css"
+  },
+  {
+    "revision": "998a946ac294a83f4834",
+    "url": "./static/js/2.6f45c638.chunk.js"
+  },
+  {
+    "revision": "29b7625fe7fa5ca90218",
+    "url": "./static/js/main.65af57e5.chunk.js"
+  },
+  {
+    "revision": "14f92f01120b5547524c",
+    "url": "./static/js/runtime-main.e4c2ba47.js"
+  }
+]);
